a4d277c345418dac6b95f8ae012228ad8557ad9d
1132892414@qq.com <1132892414@qq.com>
2017-02-22 00:36:27 +0800

    Adjustment of structure.

--- src/java/main/org/kay/com/alogrithm/linkeds/SortedLinked.java
+++ src/java/main/org/kay/com/alogrithm/linkeds/SortedLinked.java
@@ -23,75 +23,75 @@ package org.kay.com.alogrithm.linkeds;
  */
 public class SortedLinked {
 
-    private class Note {
-        Object obj;
-        Note next = null;
+	private class Note {
+		Object obj;
+		Note next = null;
 
-        Note(Object obj) {
-            this.obj = obj;
-        }
-    }
+		Note(Object obj) {
+			this.obj = obj;
+		}
+	}
 
-    private Note first;
+	private Note first;
 
-    private void insert(Object obj) {
-        if (obj == null) {
-            throw new LinkedNoteException("obj cannot be null!");
-        }
+	private void insert(Object obj) {
+		if (obj == null) {
+			throw new LinkedNoteException("obj cannot be null!");
+		}
 
-        Note note = new Note(obj);
-        Note pre  = null;
-        Note cur  = first;
-        while (cur != null && (Integer.valueOf(note.obj.toString()) > Integer.valueOf(cur.obj.toString()))) {
-            pre = cur;
-            cur = cur.next;
-        }
+		Note note = new Note(obj);
+		Note pre = null;
+		Note cur = first;
+		while (cur != null && (Integer.valueOf(note.obj.toString()) > Integer.valueOf(cur.obj.toString()))) {
+			pre = cur;
+			cur = cur.next;
+		}
 
-        if (pre == null) {
-            first = note;
-        }
-        else {
-            pre.next = note;
-        }
-        note.next = cur;
-    }
+		if (pre == null) {
+			first = note;
+		}
+		else {
+			pre.next = note;
+		}
+		note.next = cur;
+	}
 
-    private Object delete() {
-        if (first == null) {
-            throw new LinkedNoteException("SortedLinked is empty!");
-        }
+	private Object delete() {
+		if (first == null) {
+			throw new LinkedNoteException("SortedLinked is empty!");
+		}
 
-        Note temp = first;
-        first = first.next;
-        return temp.obj;
-    }
+		Note temp = first;
+		first = first.next;
+		return temp.obj;
+	}
 
-    private void display() {
-        if (first == null) {
-            throw new LinkedNoteException("SortedLinked is empty!");
-        }
+	private void display() {
+		if (first == null) {
+			throw new LinkedNoteException("SortedLinked is empty!");
+		}
 
-        System.out.print("first -> last : ");
-        Note cur = first;
-        while (cur != null) {
-            System.out.print(cur.obj.toString() + " -> ");
-            cur = cur.next;
-        }
-        System.out.println();
-    }
+		System.out.print("first -> last : ");
+		Note cur = first;
+		while (cur != null) {
+			System.out.print(cur.obj.toString() + " -> ");
+			cur = cur.next;
+		}
+		System.out.println();
+	}
 
-    public static void main(String[] args) {
-        SortedLinked linked = new SortedLinked();
-        linked.insert(80);
-        linked.insert(2);
-        linked.insert(100);
-        linked.display();
-        System.out.println("first -> last : " + linked.delete());
+	public static void main(String[] args) {
+		SortedLinked linked = new SortedLinked();
+		linked.insert(80);
+		linked.insert(2);
+		linked.insert(100);
+		linked.display();
+		System.out.println("first -> last : " + linked.delete());
 
-        linked.insert(33);
-        linked.display();
+		linked.insert(33);
+		linked.display();
 
-        linked.insert(99);
-        linked.display();
-    }
+		linked.insert(99);
+		linked.display();
+	}
 }
