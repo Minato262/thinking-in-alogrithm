25b15420793679946962849401d522cee37a42fd
1132892414@qq.com <1132892414@qq.com>
2017-05-01 00:28:09 +0800

    update test.

--- src/main/test/org/kay/com/alogrithc/UrlRandomTest.java
+++ src/main/test/org/kay/com/alogrithc/UrlRandomTest.java
@@ -11,70 +11,70 @@ import static java.lang.System.out;
  */
 public class UrlRandomTest {
 
-    @Test
-    public void test() {
-        UrlRandom randomTest = new UrlRandom();
+	@Test
+	public void test() {
+		UrlRandom randomTest = new UrlRandom();
 
-        long startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            final String url = randomTest.generate();
-            if (i % 100000 == 0){
-                out.println(url);
-            }
-        }
-        long endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
+		long startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			String url = randomTest.generate();
+			if (i % 100000 == 0) {
+				out.println(url);
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            randomTest.generate();
-        }
-        endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
-    }
+		startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			randomTest.generate();
+		}
+		endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
+	}
 
-    @Test
-    public void test1() {
-        UrlRandom randomTest = new UrlRandom(100);
+	@Test
+	public void test1() {
+		UrlRandom randomTest = new UrlRandom(100);
 
-        long startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            final String url = randomTest.generate();
-            if (i % 100000 == 0){
-                out.println(url);
-            }
-        }
-        long endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
+		long startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			String url = randomTest.generate();
+			if (i % 100000 == 0) {
+				out.println(url);
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            randomTest.generate();
-        }
-        endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
-    }
+		startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			randomTest.generate();
+		}
+		endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
+	}
 
-    @Test
-    public void test5() {
-        UrlRandom randomTest = new UrlRandom(500);
+	@Test
+	public void test5() {
+		UrlRandom randomTest = new UrlRandom(500);
 
-        long startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            final String url = randomTest.generate();
-            if (i % 100000 == 0){
-                out.println(url);
-            }
-        }
-        long endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
+		long startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			String url = randomTest.generate();
+			if (i % 100000 == 0) {
+				out.println(url);
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
-        for (int i = 1000000; i > 0; i--) {
-            randomTest.generate();
-        }
-        endTime = System.currentTimeMillis();
-        out.println("内外耗时：" + (endTime - startTime) + "ms");
-    }
+		startTime = System.currentTimeMillis();
+		for (int i = 1000000; i > 0; i--) {
+			randomTest.generate();
+		}
+		endTime = System.currentTimeMillis();
+		out.println("内外耗时：" + (endTime - startTime) + "ms");
+	}
 
 }
