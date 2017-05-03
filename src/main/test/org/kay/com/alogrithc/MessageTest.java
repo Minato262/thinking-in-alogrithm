648e9b114e4140b18f26a3028834f288aff0b77a
1132892414@qq.com <1132892414@qq.com>
2017-05-03 23:39:02 +0800

    update test.

--- src/main/test/org/kay/com/alogrithc/MessageTest.java
+++ src/main/test/org/kay/com/alogrithc/MessageTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kay.com.collection.array;
+package org.kay.com.alogrithc;
 
 import org.junit.Test;
 
