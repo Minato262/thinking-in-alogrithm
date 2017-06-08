625af73aed7623d23a99a7e38531419a4889cf43
kay <1132892414@qq.com>
2017-06-08 23:49:52 +0800

    添加注释。

---
+++ src/main/java/org/kay/com/cocurrent/juc/SegmentLock.java
@@ -0,0 +1,43 @@
+package org.kay.com.cocurrent.juc;
+
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 分段锁，系统提供一定数量的原始锁，根据传入对象的哈希值获取对应的锁并加锁
+ * 注意：要锁的对象的哈希值如果发生改变，有可能导致锁无法成功释放!!!
+ *
+ * @author kay
+ * @version v0.0.1
+ */
+public class SegmentLock<T> {
+    private Integer segments = 16;//默认分段数量
+    private final HashMap<Integer, ReentrantLock> lockMap = new HashMap<>();
+
+    public SegmentLock() {
+        init(null, false);
+    }
+
+    public SegmentLock(Integer counts, boolean fair) {
+        init(counts, fair);
+    }
+
+    private void init(Integer counts, boolean fair) {
+        if (counts != null) {
+            segments = counts;
+        }
+        for (int i = 0; i < segments; i++) {
+            lockMap.put(i, new ReentrantLock(fair));
+        }
+    }
+
+    public void lock(T key) {
+        ReentrantLock lock = lockMap.get(key.hashCode() % segments);
+        lock.lock();
+    }
+
+    public void unlock(T key) {
+        ReentrantLock lock = lockMap.get(key.hashCode() % segments);
+        lock.unlock();
+    }
+}
---
+++ src/main/java/org/kay/com/cocurrent/juc/WeakHashLock.java
@@ -0,0 +1,54 @@
+package org.kay.com.cocurrent.juc;
+
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 弱引用锁，为每个独立的哈希值提供独立的锁功能.
+ *
+ * @author kay
+ * @version v0.0.1
+ */
+public class WeakHashLock<T> {
+    private ConcurrentHashMap<T, WeakLockRef<T, ReentrantLock>> lockMap = new ConcurrentHashMap<>();
+    private ReferenceQueue<ReentrantLock> queue = new ReferenceQueue<>();
+
+    public ReentrantLock get(T key) {
+        if (lockMap.size() > 1000) {
+            clearEmptyRef();
+        }
+        WeakReference<ReentrantLock> lockRef = lockMap.get(key);
+        ReentrantLock lock = (lockRef == null ? null : lockRef.get());
+        while (lock == null) {
+            lockMap.putIfAbsent(key, new WeakLockRef<>(new ReentrantLock(), queue, key));
+            lockRef = lockMap.get(key);
+            lock = (lockRef == null ? null : lockRef.get());
+            if (lock != null) {
+                return lock;
+            }
+            clearEmptyRef();
+        }
+        return lock;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void clearEmptyRef() {
+        Reference<? extends ReentrantLock> ref;
+        while ((ref = queue.poll()) != null) {
+            WeakLockRef<T, ? extends ReentrantLock> weakLockRef = (WeakLockRef<T, ? extends ReentrantLock>) ref;
+            lockMap.remove(weakLockRef.key);
+        }
+    }
+
+    private static final class WeakLockRef<T, K> extends WeakReference<K> {
+        final T key;
+
+        private WeakLockRef(K referent, ReferenceQueue<? super K> q, T key) {
+            super(referent, q);
+            this.key = key;
+        }
+    }
+}
