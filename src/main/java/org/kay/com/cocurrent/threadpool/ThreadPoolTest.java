04b3752281f237b221a8f06be228f801745f34e8
kay <1132892414@qq.com>
2018-06-09 16:13:47 +0800

    添加 queue

---
+++ src/main/java/org/kay/com/cocurrent/threadpool/ThreadPoolTest.java
@@ -0,0 +1,36 @@
+package org.kay.com.cocurrent.threadpool;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author kay
+ * @version v1.0
+ */
+public class ThreadPoolTest implements Runnable {
+
+    private int i;
+
+    public ThreadPoolTest(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 6, 1, TimeUnit.DAYS, queue);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new ThreadPoolTest(i));
+            int threadSize = queue.size();
+            System.out.println("线程队列大小为-->" + threadSize);
+        }
+        executor.shutdown();
+    }
+}
