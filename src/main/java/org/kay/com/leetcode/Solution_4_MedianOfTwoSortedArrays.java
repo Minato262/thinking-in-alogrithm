956c89c5b8b3621c0967985ca8b407e8190b88be
kay <1132892414@qq.com>
2019-03-24 19:07:49 +0800

    init.

--- src/main/java/org/kay/com/leetcode/Solution_4_MedianOfTwoSortedArrays.java
+++ src/main/java/org/kay/com/leetcode/Solution_4_MedianOfTwoSortedArrays.java
@@ -1,4 +1,29 @@
 package org.kay.com.leetcode;
 
+/**
+ * 4. Median of Two Sorted Arrays
+ * <p>
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ * You may assume nums1 and nums2 cannot be both empty.
+ * <p>
+ * Example 1:
+ *  nums1 = [1, 3]
+ *  nums2 = [2]
+ *  The median is 2.0
+ * <p>
+ * Example 2:
+ *  nums1 = [1, 2]
+ *  nums2 = [3, 4]
+ *  The median is (2 + 3)/2 = 2.5
+ */
 public class Solution_4_MedianOfTwoSortedArrays {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0L;
+    }
 }
---
+++ src/main/java/org/kay/com/leetcode/Solution_7_ReverseInteger.java
@@ -0,0 +1,34 @@
+package org.kay.com.leetcode;
+
+/**
+ * 7.Reverse Integer
+ * <p>
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * <p>
+ * Example 1:
+ *  Input: 123
+ *  Output: 321
+ * <p>
+ * Example 2:
+ *  Input: -123
+ *  Output: -321
+ * <p>
+ * Example 3:
+ *  Input: 120
+ *  Output: 21
+ * <p>
+ * Note:
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer
+ * range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the
+ * reversed integer overflows.
+ */
+public class Solution_7_ReverseInteger {
+
+    public static void main() {
+
+    }
+
+    private int reverse(int x) {
+        return 0;
+    }
+}
