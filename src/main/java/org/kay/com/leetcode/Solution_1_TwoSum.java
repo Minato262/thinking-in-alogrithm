bb7bd1e1c2fb4b58cf47c96b261e64c23ab251d4
kay <1132892414@qq.com>
2019-03-24 18:54:53 +0800

    init.

--- src/main/java/org/kay/com/leetcode/Solution_1_TwoSum.java
+++ src/main/java/org/kay/com/leetcode/Solution_1_TwoSum.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Two Sum
+ * 1_Two Sum
  * <p>
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * <p>
  * Example:
- * Given nums = [2, 7, 11, 15], target = 9,
+ *      Given nums = [2, 7, 11, 15], target = 9,
  * <p>
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
+ *      Because nums[0] + nums[1] = 2 + 7 = 9,
+ *      return [0, 1].
  */
 public class Solution_1_TwoSum {
 
--- src/main/java/org/kay/com/leetcode/Solution_2_AddTwoNumbers.java
+++ src/main/java/org/kay/com/leetcode/Solution_2_AddTwoNumbers.java
@@ -1,38 +1,48 @@
 package org.kay.com.leetcode;
 
 /**
- * Add Two Numbers
+ * 2_Add Two Numbers
  * <p>
- * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in
- * reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked
- * list.
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single
+ * digit. Add the two numbers and return it as a linked list.
  * <p>
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * You may assume the two numbers do not contain any leading zero, except the number
+ * 0 itself.
  * <p>
  * Example:
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
- * Output: 7 -> 0 -> 8
- * Explanation: 342 + 465 = 807.
- *
+ *      Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *      Output: 7 -> 0 -> 8
+ * Explanation:
+ *      342 + 465 = 807.
  * <p>
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
+ *    int val;
+ *    ListNode next;
+ *    ListNode(int x) {
+ *      val = x;
+ *    }
  * }
  */
 public class Solution_2_AddTwoNumbers {
 
     public static void main(String[] args) {
+        ListNode listNode0 = new ListNode(0);
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
 
+        listNode0.next = listNode1;
+        listNode1.next = listNode2;
+
+        System.out.println(listNode0);
     }
 
     private ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return null;
     }
 
-    public class ListNode {
+    private static class ListNode {
         int      val;
         ListNode next;
 
